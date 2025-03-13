b0c5b7d0bb4cdc883d1248a1cb82e54a45d316e2
Fayshal007 <fayshalislam5@gmail.com>
2025-03-13 16:59:04 +0600

    task array, emplt input field validation,duplicate task validation, new and old task store together

--- src/projects/ToDo/Todo.jsx
+++ src/projects/ToDo/Todo.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTask] = useState([]);
+
+  // ---------------handleInputField---------------
+  const handleInputField = (value) => {
+    setInputValue(value);
+  };
+
+  // ---------------handleSubmit---------------
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //-------------- checking if the inputfield is empty or not
+    if (!inputValue) {
+        alert("Please insert a task")
+        return
+    }
+    //----------------- checking the same task is included or not
+    if (tasks.includes(inputValue)) {
+        alert("Same task is already added")
+        setInputValue('')
+        return
+    }
+    //---------------- setting the task array
+    setTask((prev) => [...prev, inputValue])
+    //---------------- clearing the input field
+    setInputValue('')
+  };
+
   return (
     <>
       <section className="mx-auto h-screen bg-emerald-900 flex flex-col items-center">
@@ -22,14 +50,18 @@ const Todo = () => {
         </header>
         {/*---------------------------------------- input field ----------------------------------------*/}
         <section className="my-3">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex gap-1">
               <input
                 type="text"
+                value={inputValue}
                 placeholder="write a task"
                 className="input input-md"
+                onChange={(e) => handleInputField(e.target.value)}
               />
-              <button className="btn btn-success">Add Task</button>
+              <button type="submit" className="btn btn-success">
+                Add Task
+              </button>
             </div>
           </form>
         </section>
