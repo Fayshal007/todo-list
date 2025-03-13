13506352fbf46e8abf902943d40d36274c973417
Fayshal007 <fayshalislam5@gmail.com>
2025-03-13 16:06:55 +0600

    header done

---
+++ src/projects/ToDo/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <>
+      <section className="mx-auto h-screen bg-emerald-900 flex flex-col items-center">
+        <header className="text-white">
+            <h1 className="text-7xl underline text-center font-bold">ToDo List</h1>
+            <p className="text-center mt-2">by <a className="text-blue-500 underline" target="_blank" href="https://www.facebook.com/fayshal.islam.9">Fayshal Islam</a></p>
+        </header>
+      </section>
+    </>
+  );
+};
+
+export default Todo;
