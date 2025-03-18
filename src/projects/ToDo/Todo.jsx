5495654678b0fbe624bcf5a77ea18bbc92447c15
Fayshal007 <fayshalislam5@gmail.com>
2025-03-18 23:04:40 +0600

    date and time auto update done

--- src/projects/ToDo/Todo.jsx
+++ src/projects/ToDo/Todo.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
 const Todo = () => {
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTask] = useState([]);
+  const [dateTime, setDateTime] = useState("");
+  
 
   // ---------------handleInputField---------------
   const handleInputField = (value) => {
@@ -30,7 +32,16 @@ const Todo = () => {
     //---------------- clearing the input field
     setInputValue("");
   };
-
+  // ---------------Date and Time---------------
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formatedDate = now.toLocaleDateString();
+      const formatedTime = now.toLocaleTimeString();
+      setDateTime(`${formatedDate} - ${formatedTime}`);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
       <section className="mx-auto h-screen bg-emerald-900 flex flex-col items-center">
@@ -49,6 +60,8 @@ const Todo = () => {
               Fayshal Islam
             </a>
           </p>
+          <h3 className="font-bold text-2xl text-center">Date - Time</h3>
+          <h3 className="font-bold text-xl text-center">{dateTime}</h3>
         </header>
         {/*---------------------------------------- input field ----------------------------------------*/}
         <section className="my-3">
@@ -72,16 +85,24 @@ const Todo = () => {
         <section>
           <ul className="list bg-base-100 w-[300px] rounded-box shadow-md">
             {tasks.map((task, index) => (
-              <li className="list-row flex justify-between items-center" key={index}>
+              <li
+                className="list-row flex justify-between items-center"
+                key={index}
+              >
                 <p>{task}</p>
                 <div>
-                    <button className="btn bg-green-600"><FaCheck className="text-white text-xl"/></button>
-                    <button className="btn bg-red-600 "><MdDelete  className="text-white text-xl"/></button>
+                  <button className="btn bg-green-600">
+                    <FaCheck className="text-white text-xl" />
+                  </button>
+                  <button className="btn bg-red-600 ">
+                    <MdDelete className="text-white text-xl" />
+                  </button>
                 </div>
               </li>
             ))}
           </ul>
         </section>
+        <button className="btn btn-active btn-error">Clear All Task</button>
       </section>
     </>
   );
